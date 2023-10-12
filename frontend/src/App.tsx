f7fdd8e1d6c5189cc5fa1860be41840c0a8524bd
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-11 22:30:39 -0500

    MainLayout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainLayout from './MainLayout/MainLayout';
 
 let IsAuthorized = true;
 
@@ -8,20 +8,7 @@ function App() {
   return (
     <div className="App">
       {IsAuthorized ? (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MainLayout />
       ) : (
         <p>No está autorizado</p>
       )}
