baea9d1659ee4d1c822d058d5cea850804527184
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-12 20:24:55 -0500

    Grid

--- frontend/src/MainLayout/MainLayout.tsx
+++ frontend/src/MainLayout/MainLayout.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import ProjectHeader from "../ProjectHeader/ProjectHeader";
 import Layout, { Content, Header } from "antd/es/layout/layout";
+import { Col, Row } from "antd";
 
 
 function MainLayout() {
     return (
         <React.Fragment>
             <Layout style={{ minHeight: '100vh' }}>
-                <Layout>
-                    <Header className="site-layout-background" style={{ padding: 0 }}>
-                        <ProjectHeader />
-                    </Header>
-                </Layout>
-                <Content style={{ margin: '24px 16px' }}>
-                    Content
-                </Content>
+                <Row>
+                    <Col span={24}>
+                        <Header className="site-layout-background" style={{ padding: 0 }}>
+                            <ProjectHeader />
+                        </Header>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Content style={{ margin: '24px 16px' }}>
+                            Content
+                        </Content>
+                    </Col>
+                </Row>
             </Layout>
         </React.Fragment>
     )
--- frontend/src/ProjectHeader/ProjectHeader.tsx
+++ frontend/src/ProjectHeader/ProjectHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectHeader() {
     return (
         <React.Fragment>
-            <h1>Header tal</h1>
+            <span>Header</span>
         </React.Fragment>
     )
 }
