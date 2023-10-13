bf82befee30aeea9511f2c0d6763326d71e9fcb9
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-12 22:17:21 -0500

    Error manager

---
+++ frontend/src/ErrorManager/ErrorManager.tsx
@@ -0,0 +1,23 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import React from "react";
+
+export default function ErrorManager() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const error = location.state as {
+    statusText: string;
+    message: string;
+  };
+
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error?.statusText || error?.message}</i>
+      </p>
+    </div>
+  );
+}
--- frontend/src/router-manager.tsx
+++ frontend/src/router-manager.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./Home/Home";
 import Ejemplo from "./Ejemplo/Ejemplo";
+import ErrorManager from './ErrorManager/ErrorManager';
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+      errorElement: <ErrorManager />
     },
     {
       path: "/artesano",
       element: <Ejemplo />,
+      errorElement: <ErrorManager />
     },
 ]);
 
