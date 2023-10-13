f5996fee8c6d4ab23c500e53a832858507d04dab
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-12 21:14:50 -0500

    Header

--- frontend/src/ProjectHeader/ProjectHeader.tsx
+++ frontend/src/ProjectHeader/ProjectHeader.tsx
@@ -2,14 +2,20 @@ import { Col, Row } from 'antd';
 import './ProyectHeader.css';
 import { Header } from "antd/es/layout/layout";
 import React from "react";
+import Search from 'antd/es/input/Search';
 
 function ProjectHeader() {
     return (
         <React.Fragment>
             <Header className="site-layout-background" style={{ padding: 0 }}>
                 <Row>
-                    <Col span={12}>SEARCH</Col>
-                    <Col span={4}>LOGO</Col>
+                    <Col span={14} className='search_container'>
+                        <Search
+                            placeholder='Buscar'
+                            allowClear
+                         />
+                    </Col>
+                    <Col span={2}>LOGO</Col>
                     <Col span={8}>ArteSampuesana</Col>
                 </Row>
             </Header>
