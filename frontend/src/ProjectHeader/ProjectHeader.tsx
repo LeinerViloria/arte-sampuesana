ee58204246bc9697499c97097304350e0f2a7af1
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-28 12:18:14 -0500

    NavBar

--- frontend/src/ProjectHeader/ProjectHeader.tsx
+++ frontend/src/ProjectHeader/ProjectHeader.tsx
@@ -4,11 +4,13 @@ import { Header } from "antd/es/layout/layout";
 import React from "react";
 import Search from 'antd/es/input/Search';
 import { Topbar } from './Topbar/Topbar';
+import NavBar from './NavBar/NavBar';
 
 function ProjectHeader() {
     return (
         <Header style={{ padding: 0 }}>
             <Topbar />
+            <NavBar />
         </Header>
     )
 }
