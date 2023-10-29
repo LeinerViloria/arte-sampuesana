44c3ba36ddb70c2b6f6fe3c597cb37c745127913
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-29 14:56:43 -0500

    Se agregó la gráfica

--- frontend/src/CraftsmenHome/CraftsmenMainDashlet/CraftsmenMainDashlet.tsx
+++ frontend/src/CraftsmenHome/CraftsmenMainDashlet/CraftsmenMainDashlet.tsx
@@ -6,19 +6,28 @@ import {
 } from '@ant-design/icons';
 import WorkspaceStatistic from './WorkspaceStatistic/WorkspaceStatistic';
 
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
 class CraftsmenMainDashlet extends Component {
     render() {
         return (
             <React.Fragment>
-                <Carousel dotPosition='bottom' autoplay>
+                <Carousel dotPosition='bottom' autoplay={true}>
                     <React.Fragment>
                         <Flex gap="middle" vertical={false} justify='center' align='center'>
                             <Statistic title="Visitas" value={1128} prefix={<EyeTwoTone />} />
-                            <Statistic title="Active Users" value={112893} />
+                            <Statistic title="Productos vendidos" value={112893} />
                         </Flex>
                     </React.Fragment>
 
-                    <WorkspaceStatistic />
+                    <React.Fragment>
+                        <Title level={3}> Los 3 articulos más vendidos en los ultimos 5 meses </Title>
+                        <div className='d-flex justify-content-center mb-3'>
+                            <WorkspaceStatistic />
+                        </div>
+                    </React.Fragment>
 
                     <React.Fragment>
                         3
--- frontend/src/CraftsmenHome/CraftsmenMainDashlet/WorkspaceStatistic/WorkspaceStatistic.tsx
+++ frontend/src/CraftsmenHome/CraftsmenMainDashlet/WorkspaceStatistic/WorkspaceStatistic.tsx
@@ -1,13 +1,84 @@
-import React, { Component } from 'react';
+import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip
+} from "recharts";
 
-class WorkspaceStatistic extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                Hola
-            </React.Fragment>
-        );
-    }
+const data = [
+  {
+    name: "Junio",
+    Hamacas: 10,
+    Sombreros: 15,
+    OtroProducto: 5,
+  },
+  {
+    name: "Julio",
+    Hamacas: 20,
+    Sombreros: 25,
+    OtroProducto: 10,
+  },
+  {
+    name: "Agosto",
+    Hamacas: 15,
+    Sombreros: 30,
+    OtroProducto: 8,
+  },
+  {
+    name: "Septiembre",
+    Hamacas: 80,
+    Sombreros: 60,
+    OtroProducto: 26,
+  },
+  {
+    name: "Octubre",
+    Hamacas: 20,
+    Sombreros: 91,
+    OtroProducto: 30,
+  }
+];
+
+export default function WorkspaceStatistic() {
+  return (
+    <AreaChart
+      width={400}
+      height={200}
+      data={data}
+      margin={{
+        top: 10,
+        right: 30,
+        left: 0,
+        bottom: 0
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Area
+        type="monotone"
+        dataKey="Hamacas"      // Cambia los nombres de los productos
+        stackId="1"
+        stroke="#8884d8"
+        fill="#8884d8"
+      />
+      <Area
+        type="monotone"
+        dataKey="Sombreros"    // Cambia los nombres de los productos
+        stackId="1"
+        stroke="#82ca9d"
+        fill="#82ca9d"
+      />
+      <Area
+        type="monotone"
+        dataKey="OtroProducto"  // Cambia los nombres de los productos
+        stackId="1"
+        stroke="#ffc658"
+        fill="#ffc658"
+      />
+    </AreaChart>
+  );
 }
-
-export default WorkspaceStatistic;
