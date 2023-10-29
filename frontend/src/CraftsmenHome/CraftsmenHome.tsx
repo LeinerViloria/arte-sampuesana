ae948674fed2f8c344d6ac0d2e7b7aeb54b44546
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-29 18:43:15 -0500

    Avance en mis productos

--- frontend/src/CraftsmenHome/CraftsmenHome.tsx
+++ frontend/src/CraftsmenHome/CraftsmenHome.tsx
@@ -8,47 +8,56 @@ import Title from 'antd/es/typography/Title';
 function CraftsmenHome()
 {
     return (
-        <Row gutter={[24, 16]}>
-            <Col span={16} className='max_height_card'>
-                <Card className='h-100' bordered={false}>
-                    <CraftsmenMainDashlet />
+        <React.Fragment>
+            <Row gutter={[24, 16]}>
+                <Col span={16} className='max_height_card'>
+                    <Card className='h-100' bordered={false}>
+                        <CraftsmenMainDashlet />
+                    </Card>
+                </Col>
+                <Col span={8} className='max_height_card'>
+                    <Row>
+                        <Card className='w-100'>
+                            <PersonalInformationDashlet />
+                        </Card>
+                    </Row>
+                    <Row>
+                        <Card className='w-100'>
+                            <Title level={5}>
+                                <a href='/artesano/culturalInformation'>
+                                    Información cultural
+                                </a>
+                            </Title>
+                        </Card>
+                    </Row>
+                    <Row>
+                        <Card className='w-100'>
+                            <Title level={5}>
+                                <a href='/artesano/productsManager'>
+                                    Administrar productos
+                                </a>
+                            </Title>
+                        </Card>
+                    </Row>
+                    <Row>
+                        <Card className='w-100'>
+                            <Title level={5}>
+                                <a href='/artesano/publication'>
+                                    Publicaciones
+                                </a>
+                            </Title>
+                        </Card>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className='mt-3'>
+                <Card className='w-100'>
+                    <Title level={4}>
+                        Mis productos
+                    </Title>
                 </Card>
-            </Col>
-            <Col span={8} className='max_height_card'>
-                <Row>
-                    <Card className='w-100'>
-                        <PersonalInformationDashlet />
-                    </Card>
-                </Row>
-                <Row>
-                    <Card className='w-100'>
-                        <Title level={5} className='mr-2'>
-                            <a href='/artesano/culturalInformation'>
-                                Información cultural
-                            </a>
-                        </Title>
-                    </Card>
-                </Row>
-                <Row>
-                    <Card className='w-100'>
-                        <Title level={5} className='mr-2'>
-                            <a href='/artesano/productsManager'>
-                                Administrar productos
-                            </a>
-                        </Title>
-                    </Card>
-                </Row>
-                <Row>
-                    <Card className='w-100'>
-                        <Title level={5} className='mr-2'>
-                            <a href='/artesano/publication'>
-                                Publicaciones
-                            </a>
-                        </Title>
-                    </Card>
-                </Row>
-            </Col>
-        </Row>
+            </Row>
+        </React.Fragment>
     );
 }
 
