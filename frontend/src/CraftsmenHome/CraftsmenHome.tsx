9099c2a8dd835bbb0fbf36c708b3b24e992cc7b5
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-29 10:39:26 -0500

    grid

--- frontend/src/CraftsmenHome/CraftsmenHome.tsx
+++ frontend/src/CraftsmenHome/CraftsmenHome.tsx
@@ -1,9 +1,22 @@
+import { Col, Layout, Row } from 'antd';
 import React from 'react';
 
 function CraftsmenHome()
 {
     return (
-        <p>Ejemplo</p>
+        <Row>
+            <Col span={16}>
+                <p>Hola</p>
+            </Col>
+            <Col span={8}>
+                <Row>
+                    <p>Hola</p>
+                </Row>
+                <Row>
+                    <p>Hola</p>
+                </Row>
+            </Col>
+        </Row>
     );
 }
 
