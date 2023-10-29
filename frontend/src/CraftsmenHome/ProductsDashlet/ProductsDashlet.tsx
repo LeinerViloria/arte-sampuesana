f20b9a531e8c41d1c4377eca5895406ab601e48f
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-29 18:59:39 -0500

    Diseño base de la tarjeta del producto

--- frontend/src/CraftsmenHome/ProductsDashlet/ProductsDashlet.tsx
+++ frontend/src/CraftsmenHome/ProductsDashlet/ProductsDashlet.tsx
@@ -1,5 +1,9 @@
+import { Card, Pagination, Image } from 'antd';
 import Title from 'antd/es/typography/Title';
 import React, { Component } from 'react';
+import {
+    StarTwoTone
+} from '@ant-design/icons';
 
 class ProductsDashlet extends Component {
     render() {
@@ -8,6 +12,21 @@ class ProductsDashlet extends Component {
                 <Title level={4}>
                     Mis productos
                 </Title>
+                <Card bordered={false} style={{
+                    width:"20%"
+                }}>
+                    <Image
+                        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                    />
+                    <Title level={5} className='mt-2'>
+                        Producto tal
+                    </Title>
+                    <p>$25000</p>
+                    <div className='d-flex w-100 justify-content-center'>
+                        <StarTwoTone />
+                    </div>
+                </Card>
+                <Pagination showQuickJumper defaultCurrent={2} total={500}/>
             </React.Fragment>
         );
     }
