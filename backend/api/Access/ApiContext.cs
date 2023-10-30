9a94f784a46b5f486cb44a51f15d619b932a8aa7
LeinerViloria <vilorialeiner2002@gmail.com>
2023-10-30 09:38:25 -0500

    required

--- backend/api/Access/ApiContext.cs
+++ backend/api/Access/ApiContext.cs
@@ -6,5 +6,10 @@ class ApiContext : DbContext
     public ApiContext(DbContextOptions<ApiContext> options)
         : base(options) { }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseMySQL("server=localhost;database=proyectoGestion;user=root;");
+    }
+
     public DbSet<Craftman> Craftman => Set<Craftman>();
 }
--- backend/api/Entities/Craftman.cs
+++ backend/api/Entities/Craftman.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Api.Entities
 {
     public class Craftman
     {
+        [Required]
         public int Rowid { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string LastName {get; set;}
     }
 }
