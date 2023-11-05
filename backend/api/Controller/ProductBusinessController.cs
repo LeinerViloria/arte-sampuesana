1c28678f86e893a529b96acd4e44149681c2f459
LeinerViloria <vilorialeiner2002@gmail.com>
2023-11-05 10:45:09 -0500

    Ajuste

--- backend/api/Controller/ProductBusinessController.cs
+++ backend/api/Controller/ProductBusinessController.cs
@@ -1,8 +1,10 @@
 using Api.Entities;
 using Api.Logic;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controller;
 
+[Route(nameof(ProductBusiness))]
 public class ProductBusinessController : ApiBaseController<ProductBusinessLogic, ProductBusiness>
 {
     public ProductBusinessController(IServiceProvider provider) : base(provider)
