55dead1c1e225488ca2f69de78790508a4cdc832
LeinerViloria <vilorialeiner2002@gmail.com>
2023-11-11 13:44:10 -0500

    QR

--- frontend/src/CraftsmenHome/CraftsmenHome.tsx
+++ frontend/src/CraftsmenHome/CraftsmenHome.tsx
@@ -1,5 +1,5 @@
-import { Button, Card, Col, Row } from 'antd';
-import React from 'react';
+import { Button, Card, Col, Modal, QRCode, Row } from 'antd';
+import React, { useState } from 'react';
 import './CraftsmenHome.tsx.css';
 import CraftsmenMainDashlet from './CraftsmenMainDashlet/CraftsmenMainDashlet';
 import PersonalInformationDashlet from './PersonalInformationDashlet/PersonalInformationDashlet';
@@ -8,6 +8,31 @@ import ProductsDashlet from './ProductsDashlet/ProductsDashlet';
 
 function CraftsmenHome()
 {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleOk = () => {
+        const canvas = document.getElementById('myqrcode')?.querySelector<HTMLCanvasElement>('canvas');
+        console.log(canvas);
+        if (canvas) {
+            const url = canvas.toDataURL();
+            const a = document.createElement('a');
+            a.download = 'QRCode.png';
+            a.href = url;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+
+        setIsModalOpen(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
     return (
         <React.Fragment>
             <Row gutter={[24, 16]}>
@@ -43,9 +68,15 @@ function CraftsmenHome()
                     <Row>
                         <Card className='w-100'>
                             <Title level={5}>
-                                <Button type="link" block>
+                                <Button type="link" block onClick={showModal}>
                                     Genera tu QR
                                 </Button>
+                                <Modal title="QR" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}
+                                okText={"Descargar"}>
+                                    <div id='myqrcode' className='w-100 d-flex justify-content-center align-items-center'>
+                                        <QRCode type="canvas" value="https://www.facebook.com/artesampues2020/" />
+                                    </div>
+                                </Modal>
                             </Title>
                         </Card>
                     </Row>
